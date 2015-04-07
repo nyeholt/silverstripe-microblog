6b031708762fb14eae5ccccf9497559988c94368
Marcus Nyeholt <marcus@silverstripe.com.au>
2015-04-07 15:57:20 +1000

    FIX Publish notices obey page permissions
    - Changed published page notices to inherit permissions from the page they're
      bound against instead of being sent to all logged_in users

--- code/extensions/PublishedPageNotice.php
+++ code/extensions/PublishedPageNotice.php
@@ -12,14 +12,29 @@ class PublishedPageNotice extends SiteTreeExtension {
 	 */
 	public $microBlogService;
 	
+	/**
+	 * Only notify about a particular page being published if it hasn't been published in the last
+	 * X number of days
+	 *
+	 * @var int
+	 */
+	public $notifyOncePer = 86400;
+	
 	public function onAfterPublish(&$original) {
+		$lastPub = strtotime($original->LastEdited);
+		
+		if (time() - $lastPub < $this->notifyOncePer) {
+			return;
+		}
+		
 		$text = <<<POST
 **%s %s** updated the page **[%s](%s)** at %s
 POST;
 		$member = Member::currentUser();
 		if ($member && $member->ID) {
+			$tgt = $this->owner->ClassName . ',' . $this->owner->ID;
 			$content = sprintf(_t('MicroBlog.PAGE_PUBlISH_NOTICE', $text), $member->FirstName, $member->Surname, $this->owner->Title, $this->owner->Link(), date('H:i M jS'));
-			$this->microBlogService->createPost($member, $content, array('PostType' => 'notice-post'), 0, null, array('logged_in' => true));
+			$this->microBlogService->createPost($member, $content, array('PostType' => 'notice-post'), 0, $tgt);
 		}
 	}
 }
